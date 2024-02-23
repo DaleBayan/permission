da9e336ae7b89705191f6e9a1cda4b3d29d04ab8
DaleBayan <christiandalebayan@gmail.com>
2024-02-23 14:07:49 +0800

    Remove the other linies

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', 'role:Super Admin'])->name('dashboard');
 
-
-
-
-
-
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
